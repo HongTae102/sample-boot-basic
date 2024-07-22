0a8d76a65f08ffb3a90472fe3bf4d0c44dd8abe5
662110197 Tae <151112817+HongTae102@users.noreply.github.com>
2024-07-22 15:54:48 +0700

    Test1

--- src/main/java/th/mfu/HelloController.java
+++ src/main/java/th/mfu/HelloController.java
@@ -1,11 +1,26 @@
 package th.mfu;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    String[] validNames = {"A", "B", "C"};
+
+    @GetMapping("/hi/{name}")
+    ResponseEntity<String> hi(@PathVariable String name){
+        // return ResponseEntity.ok("Hi "+ name);
+        // return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        for(String validName : validNames){
+            if(validName.equals(name))
+            return ResponseEntity.ok("Hi " + name);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
     @RequestMapping("/hello")
     String hello() {
